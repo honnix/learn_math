2ef5f7f006aa5905f1c8c160de06f45b4e3eeda3
ehonlia <hongxin.liang@ericsson.com>
2016-07-01 21:27:02 +0200

    initial project

---
+++ learn_math.pl
@@ -0,0 +1,20 @@
+:- use_module(library(random)).
+
+jia_fa :-
+    random(0, 10, Op1),
+    random(0, 10, Op2),
+    jia_fa0(Op1, Op2, 0, 3),
+    jia_fa.
+
+jia_fa0(Op1, Op2, X, X) :- !,
+    Result is Op1 + Op2,
+    format('~w + ~w = ~w~n', [Op1,Op2,Result]).
+jia_fa0(Op1, Op2, C, Threshold) :-
+    format('~w + ~w = ', [Op1,Op2]),
+    read_string(current_input, "\n", "\r\t ", _, String),
+    number_string(Result, String),
+    (   Result is Op1 + Op2
+    ->  true
+    ;   C1 is C + 1,
+        jia_fa0(Op1, Op2, C1, Threshold)
+    ).
