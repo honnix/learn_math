3c9c021a63b266048c468e1c1584b7c6e7ec0b79
ehonlia <hongxin.liang@ericsson.com>
2016-07-02 21:02:35 +0200

    handle non-expected input

--- learn_math.pl
+++ learn_math.pl
@@ -1,20 +1,19 @@
 :- use_module(library(random)).
 
 jia_fa :-
-    random(0, 10, Op1),
-    random(0, 10, Op2),
+    random(0, 5, Op1),
+    random(0, 5, Op2),
     jia_fa0(Op1, Op2, 0, 3),
     jia_fa.
 
 jia_fa0(Op1, Op2, X, X) :- !,
     Result is Op1 + Op2,
     format('~w + ~w = ~w~n', [Op1,Op2,Result]).
-jia_fa0(Op1, Op2, C, Threshold) :-
+jia_fa0(Op1, Op2, _, _) :-
     format('~w + ~w = ', [Op1,Op2]),
     read_string(current_input, "\n", "\r\t ", _, String),
     number_string(Result, String),
-    (   Result is Op1 + Op2
-    ->  true
-    ;   C1 is C + 1,
-        jia_fa0(Op1, Op2, C1, Threshold)
-    ).
+    Result is Op1 + Op2, !.
+jia_fa0(Op1, Op2, C, Threshold) :-
+    C1 is C + 1,
+    jia_fa0(Op1, Op2, C1, Threshold).
